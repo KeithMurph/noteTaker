eb41e3f177bd1dbbe26cfcabec90013f5f700c58
KeithMurph <keithmurphy94@gmail.com>
2021-07-23 01:58:47 -0700

    added location reload to save button

--- Develop/public/assets/js/index.js
+++ Develop/public/assets/js/index.js
@@ -1,7 +1,7 @@
 if (window.location.pathname === '/notes') {
   noteTitle = document.querySelector('.note-title');
   noteText = document.querySelector('.note-textarea');
-  saveNoteBtn = document.querySelector('.save-note');
+  saveNoteBtn = document.querySelector('.save-note', <input type="button" value="Reload Page" onClick="document.location.reload(true)"></input>);
   newNoteBtn = document.querySelector('.new-note');
   noteList = document.querySelectorAll('.list-container .list-group');
  };
@@ -172,7 +172,7 @@ const renderNoteList = async (notes) => {
 const getAndRenderNotes = () => getNotes().then(renderNoteList);
 
 if (window.location.pathname === '/notes') {
-  saveNoteBtn.addEventListener('click', handleNoteSave, reload());
+  saveNoteBtn.addEventListener('click', handleNoteSave,);
   newNoteBtn.addEventListener('click', handleNewNoteView);
   noteTitle.addEventListener('keyup', handleRenderSaveBtn);
   noteText.addEventListener('keyup', handleRenderSaveBtn);
