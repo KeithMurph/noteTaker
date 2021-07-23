2d4b4a731489042efa6eb857fa513ed1fea77c7c
KeithMurph <keithmurphy94@gmail.com>
2021-07-23 02:00:34 -0700

    removed attempted location reload to save button

--- Develop/public/assets/js/index.js
+++ Develop/public/assets/js/index.js
@@ -1,7 +1,7 @@
 if (window.location.pathname === '/notes') {
   noteTitle = document.querySelector('.note-title');
   noteText = document.querySelector('.note-textarea');
-  saveNoteBtn = document.querySelector('.save-note', <input type="button" value="Reload Page" onClick="document.location.reload(true)"></input>);
+  saveNoteBtn = document.querySelector('.save-note');
   newNoteBtn = document.querySelector('.new-note');
   noteList = document.querySelectorAll('.list-container .list-group');
  };
