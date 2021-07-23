ca97da744dd985c80c30c1ae9f2c3301f225c875
KeithMurph <keithmurphy94@gmail.com>
2021-07-22 23:10:00 -0700

    notes routes

--- server.js
+++ server.js
@@ -3,7 +3,7 @@ const path = require("path");
 const fs = require("fs");
 const util = require("util");
 
-const app = express();
+const notes = require('express').express();
 const PORT = process.env.PORT || 4000;
 
 
@@ -11,20 +11,20 @@ const readFileAsync = util.promisify(fs.readFile);
 const writeFileAsync = util.promisify(fs.writeFile);
 
 
-app.use(express.urlencoded({ extended : true}));
-app.use(express.json());
+notes.use(express.urlencoded({ extended : true}));
+notes.use(express.json());
 
-app.use(express.static("./Develop/public"));
+notes.use(express.static("./Develop/public"));
 
 
-app.get("/api/notes", function(req, res) {
+notes.get("/api/notes", function(req, res) {
     readFileAsync("./Develop/db/db.json", "utf8").then(function(data) {
         notes = [].concat(JSON.parse(data))
         res.json(notes);
     })
 });
 
-app.post("/api/notes", function(req, res) {
+notes.post("/api/notes", function(req, res) {
     const note = req.body;
     readFileAsync("./Develop/db/db.json", "utf8").then(function(data) {
         const notes = [].concat(JSON.parse(data));
@@ -38,7 +38,7 @@ app.post("/api/notes", function(req, res) {
     })
 });
 
-app.delete("/api/notes/:id", function(req, res) {
+notes.delete("/api/notes/:id", function(req, res) {
     const idToDelete = parseInt(req.params.id);
     readFileAsync("./Develop/db/db.json", "utf8").then(function(data) {
         const notes = [].concat(JSON.parse(data));
@@ -46,6 +46,7 @@ app.delete("/api/notes/:id", function(req, res) {
         for (let i = 0; i<notes.length; i++) {
             if(idToDelete !==notes[i].id) {
                 newNoteData.push(notes[i])
+                
             }
         }
         return newNoteData
@@ -60,27 +61,26 @@ app.delete("/api/notes/:id", function(req, res) {
 
 
     // html
-    app.get("/notes", function(req, res) {
+    notes.get("/notes", function(req, res) {
         res.sendFile(path.join(__dirname, "./Develop/public/notes.html"));
 
     });
 
-    app.get("/", function(req, res) {
+    notes.get("/", function(req, res) {
         res.sendFile(path.join(__dirname, "./Develop/public/index.html"));
     });
 
-    app.get("*", function(req, res) {
+    notes.get("*", function(req, res) {
         res.sendFile(path.join(__dirname, "./Develop/public/index.html"));
         
     });
 
     // listen
 
-    app.listen(PORT, function() {
+    notes.listen(PORT, function() {
         console.log("listening on port" + PORT);
         
     });
 
-
-
+    
 
